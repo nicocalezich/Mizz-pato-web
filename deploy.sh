4ddcd8165c0202ca680141d4610987fd139eec2e
nicocalezich <nicocalezich@gmail.com>
2021-08-04 23:05:07 -0300

    full app

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:nicocalezich/Web-mizzpato.git main:gh-pages
+git push -f git@github.com:nicocalezich/Mizz-pato-web.git main:gh-pages
 
 cd -
